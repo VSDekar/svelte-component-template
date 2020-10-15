d35f269e949b6b9d7ee02fc4bbc779336b66e9de
Remo Fischer <remo.fischer@firewave.io>
2020-10-15 13:18:32 +0200

    some improvements

--- electron/electron.js
+++ electron/electron.js
@@ -46,3 +46,11 @@ app.whenReady().then(() => {
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit();
 });
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
--- electron/src/routes.js
+++ electron/src/routes.js
@@ -7,10 +7,6 @@ export const routes = [
     component: Component,
   },
   {
-    path: '/component',
-    component: Component,
-  },
-  {
     path: '*',
     component: Error,
   },
